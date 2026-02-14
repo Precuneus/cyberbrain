56621ac436937c52fb1a08d6c453072444d084d7
Olgierd Borowiecki <olgierd.borowiecki@gmail.com>
2026-02-14 12:43:19 +0100

    Update paths and config for cyberbrain.pl domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://precuneus.github.io',
-  base: '/brain-week',
+  site: 'https://cyberbrain.pl',
 });
